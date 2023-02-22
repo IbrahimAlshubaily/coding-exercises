f8a1c67ce5f53732261470b1f1652d5cfebfb3e7
Ibrahim Alshubaily <ibrahim.alshubaily.sde@gmail.com>
2023-02-22 17:58:37 +0000

    dynamic fib

---
+++ fib.py
@@ -0,0 +1,12 @@
+#n =	0	1	2	3	4	5	6	7	8	9	10	11	12	13	14	
+#xn =	0	1	1	2	3	5	8	13	21	34	55	89	144	233	377	
+def fib(n: int) -> int:
+    if n < 2:
+        return n
+    prev = 0
+    curr = 1
+    for i in range(2, n + 1):
+        prev, curr = curr,  prev + curr
+        print(i, curr)
+    return curr
+print(fib(14))
